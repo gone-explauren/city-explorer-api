75b3e71a5a2128255e8dd368b2ec3b447661e5ac
Laurel Perkins <lmperkins20@gmail.com>
2023-03-19 22:34:44 -0700

    cache fix?

--- modules/movies.js
+++ modules/movies.js
@@ -3,49 +3,51 @@
 const axios = require('axios');
 let cache = require('./cache.js');
 
-async function getMovies(req,res,next) {
-  try {
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&language=en-US&include_adult=false&query=${req.query.search}`;
-    // console.log(url);
-    // console.log(req.query.search);
+async function getMovies(req, res) {
+  // try {
+  //   let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&language=en-US&include_adult=false&query=${req.query.search}`;
+  //   // console.log(url);
+  //   // console.log(req.query.search);
 
-    let resultsFromAPI = await axios.get(url);
-    console.log(resultsFromAPI.data);
+  //   let resultsFromAPI = await axios.get(url);
+  //   console.log(resultsFromAPI.data);
 
-    let movieData = resultsFromAPI.data.results.map(movie => new Movie(movie));
-    // console.log(movieData);
+  //   let movieData = resultsFromAPI.data.results.map(movie => new Movie(movie));
+  //   // console.log(movieData);
 
-    res.send(movieData);
+  //   res.send(movieData);
 
 
-  } catch (error) {
-    //console.log(error.message);
-    // next is built into express and acts as a console.log
-    next(error);
-  }
+  // } catch (error) {
+  //   //console.log(error.message);
+  //   // next is built into express and acts as a console.log
+  //   next(error);
+  // }
 
-  try {
-    let key = 'movie-' + req.query.searchQuery;
-    if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
-      console.log('Cache hit');
-    } else {
-      console.log('Cache miss');
-      let movies = await axios.get('https://api.themoviedb.org/3/search/movie', {
-        params: {
-          query: req.query.searchQuery,
-          api_key: process.env.MOVIE_API_KEY
-        }
-      });
-      let allMovies = movies.data.results.map(movie => new Movie(movie));
-      cache[key] = {};
-      cache[key].timestamp = Date.now();
-      cache[key].data = allMovies;
-    }
-    return cache[key];
-  } catch (err) {
-    Promise.resolve().then(() => {
-      throw new Error(err.message);
-    }).catch(next);
+  let searchCity = req.query.searchQuery
+  const key = 'movies';
+  let params = {
+    api_key: process.env.MOVIES_API_KEY,
+    query: searchCity
+  };
+
+  let url = `https://api.themoviedb.org/3/search/movie`;
+
+  if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
+
+    //send cache data
+    // console.log('Cache hit');
+    res.status(200).send(cache['movies'])
+
+  } else {
+
+    // console.log('Cache miss');
+    await axios.get(url, { params })
+
+      .then(resultsFromApi => resultsFromApi.data.results.map(item => new Movie(item)))
+      // .then(console.log('results from api: ', resultsFromApi))
+      .then(dataToSend => res.status(200).send(dataToSend))
+      .catch(err => console.error(err));
   }
 }
 
--- modules/weather.js
+++ modules/weather.js
@@ -3,41 +3,27 @@
 const axios = require('axios');
 let cache = require('./cache.js');
 
+// // example request: http://localhost:3001/weather?search=Seattle
+// async function getWeather(req, res, next) {
+//   // console.log('hi friend :P');
+//   try {
+//     let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&days=3&lat=${req.query.lat}&lon=${req.query.lon}`;
+//     // console.log(url);
+//     // console.log(req.query.lat, req.query.lon);
 
-// example request: http://localhost:3001/weather?search=Seattle
-async function getWeather(req, res, next) {
-  // console.log('hi friend :P');
-  try {
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&days=3&lat=${req.query.lat}&lon=${req.query.lon}`;
-    // console.log(url);
-    // console.log(req.query.lat, req.query.lon);
+//     let resultsFromAPI = await axios.get(url);
+//     // console.log(resultsFromAPI.data);
 
-    let resultsFromAPI = await axios.get(url);
-    // console.log(resultsFromAPI.data);
+//     let forecastData = resultsFromAPI.data.data.map(forecast => new Forecast(forecast));
+//     // console.log(forecastData);
 
-    let forecastData = resultsFromAPI.data.data.map(forecast => new Forecast(forecast));
-    // console.log(forecastData);
+//     res.send(forecastData);
 
-    res.send(forecastData);
-
-  } catch (error) {
-    // next is built into express and acts as a console.log
-    next(error);
-  }
-
-  const key = 'weather-' + lat + lon;
-
-  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
-    console.log('Cache hit');
-  } else {
-    console.log('Cache miss');
-    cache[key] = {};
-    cache[key].timestamp = Date.now();
-    cache[key].data = axios.get(url).then(response => parseWeather(response.data));
-  }
-  return cache[key].data;
-
-}
+//   } catch (error) {
+//     // next is built into express and acts as a console.log
+//     next(error);
+//   }
+// }
 
 // can't say I fully understand cache but I hope this works...
 function getWeather(lat, lon) {
