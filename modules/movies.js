25252fabd07b8400193d5c09d672a9c240869701
Laurel Perkins <lmperkins20@gmail.com>
2023-03-19 21:39:04 -0700

    cache working?

--- modules/movies.js
+++ modules/movies.js
@@ -1,7 +1,7 @@
 'use-strict';
 
 const axios = require('axios');
-// let cache = require('./cache.js');
+let cache = require('./cache.js');
 
 async function getMovies(req,res,next) {
   try {
@@ -17,39 +17,37 @@ async function getMovies(req,res,next) {
 
     res.send(movieData);
 
+
   } catch (error) {
     //console.log(error.message);
     // next is built into express and acts as a console.log
     next(error);
   }
-}
 
-// // can't say I fully understand cache but I hope this works
-// async function getMovies(req, next) {
-//   try {
-//     let key = 'movie-' + req.query.searchQuery;
-//     if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
-//       console.log('Cache hit');
-//     } else {
-//       console.log('Cache miss');
-//       let movies = await axios.get('https://api.themoviedb.org/3/search/movie', {
-//         params: {
-//           query: req.query.searchQuery,
-//           api_key: process.env.MOVIE_API_KEY
-//         }
-//       });
-//       let allMovies = movies.data.results.map(movie => new Movie(movie));
-//       cache[key] = {};
-//       cache[key].timestamp = Date.now();
-//       cache[key].data = allMovies;
-//     }
-//     return cache[key];
-//   } catch (err) {
-//     Promise.resolve().then(() => {
-//       throw new Error(err.message);
-//     }).catch(next);
-//   }
-// }
+  try {
+    let key = 'movie-' + req.query.searchQuery;
+    if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
+      console.log('Cache hit');
+    } else {
+      console.log('Cache miss');
+      let movies = await axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          query: req.query.searchQuery,
+          api_key: process.env.MOVIE_API_KEY
+        }
+      });
+      let allMovies = movies.data.results.map(movie => new Movie(movie));
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
+      cache[key].data = allMovies;
+    }
+    return cache[key];
+  } catch (err) {
+    Promise.resolve().then(() => {
+      throw new Error(err.message);
+    }).catch(next);
+  }
+}
 
 class Movie {
   constructor(movieData) {
--- modules/weather.js
+++ modules/weather.js
@@ -1,7 +1,7 @@
 'use-strict';
 
 const axios = require('axios');
-// let cache = require('./cache.js');
+let cache = require('./cache.js');
 
 
 // example request: http://localhost:3001/weather?search=Seattle
@@ -24,35 +24,48 @@ async function getWeather(req, res, next) {
     // next is built into express and acts as a console.log
     next(error);
   }
+
+  const key = 'weather-' + lat + lon;
+
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url).then(response => parseWeather(response.data));
+  }
+  return cache[key].data;
+
 }
 
-// // can't say I fully understand cache but I hope this works...
-// function getWeather(lat, lon) {
-//   const key = 'weather-' + lat + lon;
-//   let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&days=3&lat=${lat}&lon=${lon}`;
+// can't say I fully understand cache but I hope this works...
+function getWeather(lat, lon) {
+  const key = 'weather-' + lat + lon;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&days=3&lat=${lat}&lon=${lon}`;
 
-//   if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
-//     console.log('Cache hit');
-//   } else {
-//     console.log('Cache miss');
-//     cache[key] = {};
-//     cache[key].timestamp = Date.now();
-//     cache[key].data = axios.get(url).then(response => parseWeather(response.data));
-//   }
-//   return cache[key].data;
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url).then(response => parseWeather(response.data));
+  }
+  return cache[key].data;
 
-// }
+}
 
-// function parseWeather(weatherData) {
-//   try {
-//     const weatherSummaries = weatherData.data.map(day => {
-//       return new Forecast(day);
-//     });
-//     return Promise.resolve(weatherSummaries);
-//   } catch (e) {
-//     return Promise.reject(e);
-//   }
-// }
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Forecast(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
 
 class Forecast {
   constructor(forecastData) {
